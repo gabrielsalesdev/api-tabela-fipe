fad7119c69c23706ca37b6cf431b72735e0e7993
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-10 10:40:24 -0300

    feat(fipe.js): implementando o controller de consulta através do código Fipe

--- src/controllers/fipe.js
+++ src/controllers/fipe.js
@@ -1,1 +1,15 @@
 const { fipeApiServices } = require('../services/index');
+
+const getVehicleValueByFipe = async (req, res, next) => {
+    try {
+        const { fipeCode } = req.params;
+
+        const response = await fipeApiServices.requestValueByFipe(fipeCode);
+
+        return res.json(response);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { getVehicleValueByFipe };
