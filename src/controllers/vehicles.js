0bb8b08872713b3bd0463da713ed47e61966a3cd
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-10 10:47:09 -0300

    feat(vehicle.js): implementando o controller de consulta das marcas

--- src/controllers/vehicles.js
+++ src/controllers/vehicles.js
@@ -1,3 +1,15 @@
 const { fipeApiServices } = require('../services/index');
 
+const getBrands = async (req, res, next) => {
+    try {
+        const { vehicleType } = req;
 
+        const response = await fipeApiServices.requestBrands(vehicleType);
+
+        return res.status(200).json(response);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { getBrands };
