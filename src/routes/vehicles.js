e3e73d35a58df1da113b45ed09911a5963591d8b
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-10 02:01:54 -0300

    feat(vehicles.js): implementando rota de consulta dos anos modelo

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -7,5 +7,6 @@ const { vehiclesMiddlewares } = require('../middlewares/index');
 
 router.get('/:vehicle', vehiclesMiddlewares.validateType, vehicleControllers.getBrands);
 router.get('/:vehicle/:brandId', vehiclesMiddlewares.validateType, vehicleControllers.getModels);
+router.get('/:vehicle/:brandId/:modelId', vehiclesMiddlewares.validateType, vehicleControllers.getModelYears);
 
 module.exports = router;
