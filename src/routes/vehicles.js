1fe9690b7b88fcbdba991c3d2141aa3935114394
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-10 02:00:02 -0300

    feat(vehicles.js): implementando rota de consulta das marcas

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -5,4 +5,6 @@ const vehicleControllers = require('../controllers/vehicles');
 
 const { vehiclesMiddlewares } = require('../middlewares/index');
 
+router.get('/:vehicle', vehiclesMiddlewares.validateType, vehicleControllers.getBrands);
+
 module.exports = router;
