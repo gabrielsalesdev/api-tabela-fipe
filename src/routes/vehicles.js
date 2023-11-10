822ce524970a8bac637b5ca43c7eb617e6d77890
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-10 02:00:22 -0300

    feat(vehicles.js): implementando rota de consulta dos modelos

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -6,5 +6,6 @@ const vehicleControllers = require('../controllers/vehicles');
 const { vehiclesMiddlewares } = require('../middlewares/index');
 
 router.get('/:vehicle', vehiclesMiddlewares.validateType, vehicleControllers.getBrands);
+router.get('/:vehicle/:brandId', vehiclesMiddlewares.validateType, vehicleControllers.getModels);
 
 module.exports = router;
