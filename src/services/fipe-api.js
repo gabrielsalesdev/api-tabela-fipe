7e9b3990971e696391ca8b7b5070e4406995e59c
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-10 10:53:11 -0300

    feat(fipe-api.js): implementando o serviço de consulta da tabela de referência atual na API da Fipe

--- src/services/fipe-api.js
+++ src/services/fipe-api.js
@@ -1,2 +1,10 @@
 const axios = require('axios');
 const { HttpError } = require('../errors/index');
+
+const requestReferenceTable = async () => {
+    const { data } = await axios.post('http://veiculos.fipe.org.br/api/veiculos/ConsultarTabelaDeReferencia');
+
+    const response = data[0].Codigo.toString();
+
+    return response;
+};
