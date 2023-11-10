4abcac45cd53fddf77fc13194e3e98629ca1c688
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-10 01:57:08 -0300

    feat(fipe.js): implementando rota GET de consulta através do código Fipe e ano modelo

--- src/routes/fipe.js
+++ src/routes/fipe.js
@@ -3,6 +3,7 @@ const router = express();
 
 const { fipeControllers } = require('../controllers/index');
 
-router.get('/fipe/:fipeCode', fipeControllers.getVehicleValueByFipe)
+router.get('/fipe/:fipeCode', fipeControllers.getVehicleValueByFipe);
+router.get('/fipe/:fipeCode/:modelYearId', fipeControllers.getVehicleValueByFipeAndModelYear);
 
 module.exports = router;
