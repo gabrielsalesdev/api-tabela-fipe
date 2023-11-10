0121ef6138c8467184be15bfd4e4a6d02a3570ac
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-10 01:40:18 -0300

    feat(app.js): implementando o uso das rotas

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const { errorsMiddlewares } = require('./middlewares/index');
 const { fipeRoutes, vehiclesRoutes } = require('./routes/index');
 
 app.use(express.json());
+app.use(fipeRoutes);
+app.use(vehiclesRoutes);
 app.use(errorsMiddlewares.errorHandler);
 
 module.exports = app;
