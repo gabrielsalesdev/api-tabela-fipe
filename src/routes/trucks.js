3cb664db0450f5325e3955478aaddbfa1fd708dd
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:15:22 -0300

    feat(trucks.js): implementando rota de consulta do valor

--- src/routes/trucks.js
+++ src/routes/trucks.js
@@ -6,5 +6,6 @@ const { vehiclesControllers } = require('../controllers/index');
 trucksRoutes.get('/motos', vehiclesControllers.getBrands);
 trucksRoutes.get('/motos/:brandId', vehiclesControllers.getModels);
 trucksRoutes.get('/motos/:brandId/:modelId', vehiclesControllers.getModelYears);
+trucksRoutes.get('/motos/:brandId/:modelId/:modelYearId', vehiclesControllers.getValue);
 
 module.exports = trucksRoutes;
