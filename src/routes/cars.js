b09f0316713c96ef22939bf60c8478919ee21681
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:10:37 -0300

    refactor(cars.js): excluindo importação do middleware de validar tipos

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -3,11 +3,9 @@ const carsRoutes = express();
 
 const { vehiclesControllers } = require('../controllers/index');
 
-const { vehiclesMiddlewares } = require('../middlewares/index');
-
-carsRoutes.get('/carros', vehiclesMiddlewares.validateType, vehicleControllers.getBrands);
-carsRoutes.get('/carros/:brandId', vehiclesMiddlewares.validateType, vehicleControllers.getModels);
-carsRoutes.get('/carros/:brandId/:modelId', vehiclesMiddlewares.validateType, vehicleControllers.getModelYears);
-carsRoutes.get('/carros/:brandId/:modelId/:modelYearId', vehiclesMiddlewares.validateType, vehicleControllers.getValue);
+carsRoutes.get('/carros', vehicleControllers.getBrands);
+carsRoutes.get('/carros/:brandId', vehicleControllers.getModels);
+carsRoutes.get('/carros/:brandId/:modelId', vehicleControllers.getModelYears);
+carsRoutes.get('/carros/:brandId/:modelId/:modelYearId', vehicleControllers.getValue);
 
 module.exports = carsRoutes;
