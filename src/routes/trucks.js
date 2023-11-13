2d7ecd4a97ec6efc3b00dd1e6de5a4af94417363
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:32:13 -0300

    feat(trucks.js): adicionando os argumentos nos controllers

--- src/routes/trucks.js
+++ src/routes/trucks.js
@@ -3,9 +3,9 @@ const trucksRoutes = express();
 
 const { vehiclesControllers } = require('../controllers/index');
 
-trucksRoutes.get('/motos', vehiclesControllers.getBrands);
-trucksRoutes.get('/motos/:brandId', vehiclesControllers.getModels);
-trucksRoutes.get('/motos/:brandId/:modelId', vehiclesControllers.getModelYears);
-trucksRoutes.get('/motos/:brandId/:modelId/:modelYearId', vehiclesControllers.getValue);
+trucksRoutes.get('/motos', vehiclesControllers.getBrands(3));
+trucksRoutes.get('/motos/:brandId', vehiclesControllers.getModels(3));
+trucksRoutes.get('/motos/:brandId/:modelId', vehiclesControllers.getModelYears(3));
+trucksRoutes.get('/motos/:brandId/:modelId/:modelYearId', vehiclesControllers.getValue(3));
 
 module.exports = trucksRoutes;
