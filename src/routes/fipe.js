c0dc8b4e017076bba684c8074c7d657b939f0735
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 12:00:59 -0300

    style(fipe.js): alterando nome da variável

--- src/routes/fipe.js
+++ src/routes/fipe.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = express();
+const fipeRoutes = express();
 
 const { fipeControllers } = require('../controllers/index');
 
 router.get('/fipe/:fipeCode', fipeControllers.getVehicleValuesByFipe);
 router.get('/fipe/:fipeCode/:modelYearId', fipeControllers.getVehicleValueByFipeAndModelYear);
 
-module.exports = router;
+module.exports = fipeRoutes;
