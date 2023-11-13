a73b4217fe2931e4eb9510685074f4b49020318d
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:37:20 -0300

    fix(trucks.js): alterando chamada das rotas

--- src/routes/trucks.js
+++ src/routes/trucks.js
@@ -3,9 +3,9 @@ const trucksRoutes = express();
 
 const { vehiclesControllers } = require('../controllers/index');
 
-trucksRoutes.get('/motos', vehiclesControllers.getBrands(3));
-trucksRoutes.get('/motos/:brandId', vehiclesControllers.getModels(3));
-trucksRoutes.get('/motos/:brandId/:modelId', vehiclesControllers.getModelYears(3));
-trucksRoutes.get('/motos/:brandId/:modelId/:modelYearId', vehiclesControllers.getValue(3));
+trucksRoutes.get('/caminhoes', vehiclesControllers.getBrands(3));
+trucksRoutes.get('/caminhoes/:brandId', vehiclesControllers.getModels(3));
+trucksRoutes.get('/caminhoes/:brandId/:modelId', vehiclesControllers.getModelYears(3));
+trucksRoutes.get('/caminhoes/:brandId/:modelId/:modelYearId', vehiclesControllers.getValue(3));
 
 module.exports = trucksRoutes;
