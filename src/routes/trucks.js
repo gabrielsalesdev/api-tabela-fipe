616711a6e3cb9cad5db5d8abc35d6f9058369338
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:14:38 -0300

    feat(trucks.js): implementando rota de consulta das marcas

--- src/routes/trucks.js
+++ src/routes/trucks.js
@@ -3,4 +3,6 @@ const trucksRoutes = express();
 
 const { vehiclesControllers } = require('../controllers/index');
 
+trucksRoutes.get('/motos', vehiclesControllers.getBrands);
+
 module.exports = trucksRoutes;
