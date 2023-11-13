588a87e33bfaa3ebe17627f74850fde9458fffc0
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:01:20 -0300

    fix(motorcycles.js): alterando nome da variável antiga

--- src/routes/motorcycles.js
+++ src/routes/motorcycles.js
@@ -4,6 +4,6 @@ const motorcyclesRoutes = express();
 const { vehiclesControllers } = require('../controllers/index');
 
 motorcyclesRoutes.get('/motos', vehiclesControllers.getBrands);
-vehiclesRoutes.get('/motos/:brandId', vehiclesControllers.getModels);
+motorcyclesRoutes.get('/motos/:brandId', vehiclesControllers.getModels);
 
 module.exports = motorcyclesRoutes;
