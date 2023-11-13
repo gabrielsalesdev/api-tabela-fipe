3097743884284cb7525c7c466c9aa3cba9c23484
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:01:54 -0300

    feat(motorcycles.js): implementando rota de consulta dos anos modelo

--- src/routes/motorcycles.js
+++ src/routes/motorcycles.js
@@ -5,5 +5,6 @@ const { vehiclesControllers } = require('../controllers/index');
 
 motorcyclesRoutes.get('/motos', vehiclesControllers.getBrands);
 motorcyclesRoutes.get('/motos/:brandId', vehiclesControllers.getModels);
+motorcyclesRoutes.get('/motos/:brandId/:modelId', vehiclesControllers.getModelYears);
 
 module.exports = motorcyclesRoutes;
