35e6bfee207d5bee4cb434bf5c48e33d16bba74b
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:21:32 -0300

    feat(app.js): importando novas rotas

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 const { errorsMiddlewares } = require('./middlewares/index');
-const { fipeRoutes, vehiclesRoutes } = require('./routes/index');
+
+const { fipeRoutes, carsRoutes, motorcyclesRoutes, trucksRoutes } = require('./routes/index');
 
 app.use(express.json());
-app.use(fipeRoutes);
-app.use(vehiclesRoutes);
+app.use(fipeRoutes, carsRoutes, motorcyclesRoutes, trucksRoutes);
 app.use(errorsMiddlewares.errorHandler);
 
 module.exports = app;
