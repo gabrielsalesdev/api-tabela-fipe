084120942f6ee19fb2c15b5225c366b744157b40
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:08:17 -0300

    style(cars.js): alterando nome das variáveis

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const vehiclesRoutes = express();
+const carsRoutes = express();
 
 const vehicleControllers = require('../controllers/vehicles');
 
 const { vehiclesMiddlewares } = require('../middlewares/index');
 
-vehiclesRoutes.get('/:vehicle', vehiclesMiddlewares.validateType, vehicleControllers.getBrands);
-vehiclesRoutes.get('/:vehicle/:brandId', vehiclesMiddlewares.validateType, vehicleControllers.getModels);
-vehiclesRoutes.get('/:vehicle/:brandId/:modelId', vehiclesMiddlewares.validateType, vehicleControllers.getModelYears);
-vehiclesRoutes.get('/:vehicle/:brandId/:modelId/:modelYearId', vehiclesMiddlewares.validateType, vehicleControllers.getValue);
+carsRoutes.get('/:vehicle', vehiclesMiddlewares.validateType, vehicleControllers.getBrands);
+carsRoutes.get('/:vehicle/:brandId', vehiclesMiddlewares.validateType, vehicleControllers.getModels);
+carsRoutes.get('/:vehicle/:brandId/:modelId', vehiclesMiddlewares.validateType, vehicleControllers.getModelYears);
+carsRoutes.get('/:vehicle/:brandId/:modelId/:modelYearId', vehiclesMiddlewares.validateType, vehicleControllers.getValue);
 
-module.exports = vehiclesRoutes;
+module.exports = carsRoutes;
