98814626488d2ae478d3309ce666a88060b15008
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:00:50 -0300

    feat(motorcycles.js): implementando rota de consulta dos modelos

--- src/routes/motorcycles.js
+++ src/routes/motorcycles.js
@@ -4,5 +4,6 @@ const motorcyclesRoutes = express();
 const { vehiclesControllers } = require('../controllers/index');
 
 motorcyclesRoutes.get('/motos', vehiclesControllers.getBrands);
+vehiclesRoutes.get('/motos/:brandId', vehiclesControllers.getModels);
 
 module.exports = motorcyclesRoutes;
