cef905eb746b8147bab08f800fde67f1bd963592
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-13 15:02:21 -0300

    feat(motorcycles.js): implementando rota de consulta do valor

--- src/routes/motorcycles.js
+++ src/routes/motorcycles.js
@@ -6,5 +6,6 @@ const { vehiclesControllers } = require('../controllers/index');
 motorcyclesRoutes.get('/motos', vehiclesControllers.getBrands);
 motorcyclesRoutes.get('/motos/:brandId', vehiclesControllers.getModels);
 motorcyclesRoutes.get('/motos/:brandId/:modelId', vehiclesControllers.getModelYears);
+motorcyclesRoutes.get('/motos/:brandId/:modelId/:modelYearId', vehiclesControllers.getValue);
 
 module.exports = motorcyclesRoutes;
