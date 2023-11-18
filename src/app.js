52874564603207f66cd74c0467d08549d1ee7383
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-17 22:38:01 -0300

    feat(app.js): importando o swagger-ui-express

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+
+
 const { errorsMiddlewares } = require('./middlewares/index');
 
 const { fipeRoutes, carsRoutes, motorcyclesRoutes, trucksRoutes } = require('./routes/index');
 
 app.use(express.json());
+
 app.use(fipeRoutes, carsRoutes, motorcyclesRoutes, trucksRoutes);
 app.use(errorsMiddlewares.errorHandler);
 
