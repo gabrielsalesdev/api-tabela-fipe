550773125a925299710ce754b000535e42c5cb0b
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-23 20:47:20 -0300

    feat(insertions.js): implementando a função de inserir as tabelas referência

--- src/helpers/insertions.js
+++ src/helpers/insertions.js
@@ -1,2 +1,17 @@
 const knex = require('../database/knex');
 const { fipeApiServices } = require('../services/index');
+
+const insertReferenceTables = async () => {
+    try {
+        const referenceTables = await fipeApiServices.requestReferenceTables();
+
+        for (const referenceTable of referenceTables) {
+            await knex('reference_tables').insert({
+                id: referenceTable.id,
+                month: referenceTable.month
+            }).onConflict('id').ignore();
+        }
+    } catch (error) {
+        console.error(error)
+    }
+};
