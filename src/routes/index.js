b52bd9165aebd7d94fc6067b94cebe9d4c6783f4
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-24 00:36:36 -0300

    refactor(index.js): excluindo o arquivo

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const fipeRoutes = require('./fipe');
-const carsRoutes = require('./cars');
-const motorcyclesRoutes = require('./motorcycles');
-const trucksRoutes = require('./trucks');
-
-module.exports = { fipeRoutes, carsRoutes, motorcyclesRoutes, trucksRoutes };
