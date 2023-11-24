5ee9f618cefb947652b85cc2e6c896fb0e811b3e
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-24 00:25:35 -0300

    feat(server.js): implementando e inicializando a task das querys

--- src/server.js
+++ src/server.js
@@ -2,4 +2,8 @@ require('dotenv').config();
 const app = require('./app');
 const port = process.env.SERVER_PORT;
 
+const querysTask = require('./tasks/querys');
+
+querysTask.start();
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
