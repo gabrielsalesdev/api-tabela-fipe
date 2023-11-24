006653a0ee5825ce49e7d6076f2c2517dfe1b6d5
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-11-24 00:00:54 -0300

    feat(querys.js): implementando a função de select id da tabela de referência mais atual

--- src/helpers/querys.js
+++ src/helpers/querys.js
@@ -1,3 +1,13 @@
 const knex = require('../database/knex');
 
 const { fipeApiServices } = require('../services/index');
+
+const selectLatestReferenceTableId = async () => {
+    try {
+        const latestReferenceTable = await knex('reference_tables').select('*').orderBy('id', 'desc').first();
+
+        return latestReferenceTable.id;
+    } catch (error) {
+        console.log(error);
+    }
+};
