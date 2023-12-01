98a78a007639c9d4c0afb7ba581cc7fa9af214b8
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-01 12:39:43 -0300

    refactor(server.js): excluindo arquivo JavaScript para criação do arquivo Typescript)

--- src/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-const port = process.env.SERVER_PORT;
-
-const querysTask = require('./tasks/querys');
-
-querysTask.start();
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
