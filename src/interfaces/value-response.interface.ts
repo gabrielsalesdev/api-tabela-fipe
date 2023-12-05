a5c1c3d769a63710dffef0c24b6edb6e76759752
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-05 11:32:53 -0300

    feat(value-response.interface.ts): implementando a interface de resposta da requisição do valor

---
+++ src/interfaces/value-response.interface.ts
@@ -0,0 +1,13 @@
+export interface ValueResponse {
+    Valor: string,
+    Marca: string,
+    Modelo: string,
+    AnoModelo: number,
+    Combustivel: string,
+    CodigoFipe: string,
+    MesReferencia: string,
+    Autenticacao: string,
+    TipoVeiculo: number,
+    SiglaCombustivel: string,
+    DataConsulta: string
+};
