07c26b86046a54707b040d35f0c45d073f4971f0
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-05 00:29:53 -0300

    feat(models.service.ts): implementando o service de modelos

---
+++ src/services/models.service.ts
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import referenceTablesHelper from "../helpers/reference-tables.helper";
+import errorsHelper from '../helpers/errors.helper';
+import { ModelRequest } from "../interfaces/model-request.interface";
+import { ModelResponse } from '../interfaces/model-response.interface';
+import { Model } from '../interfaces/model.interface';
+
+export default class ModelsService {
+    vehicleId: number;
+    brandId: number;
+
+    constructor(vehicleId: number, brandId: number) {
+        this.vehicleId = vehicleId;
+        this.brandId = brandId;
+    }
+
+    private request = async (): Promise<ModelResponse> => {
+        try {
+            const body: ModelRequest = {
+                codigoTabelaReferencia: await referenceTablesHelper.getLatest(),
+                codigoTipoVeiculo: this.vehicleId,
+                codigoMarca: this.brandId
+            };
+
+            const { data } = await axios.post('http://veiculos.fipe.org.br/api/veiculos/ConsultarModelos', body);
+            errorsHelper.checkResponseErrors(data);
+
+            console.log(data);
+            const modelsResponse: ModelResponse = data;
+            return modelsResponse;
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    public get = async (): Promise<Model[]> => {
+        try {
+            const modelsRequest: ModelResponse = await this.request();
+
+            const models: Model[] = modelsRequest.Modelos.map(model => {
+                return {
+                    idVeiculo: this.vehicleId,
+                    idMarca: this.brandId,
+                    idModelo: model.Value,
+                    nomeModelo: model.Label
+                }
+            });
+            return models;
+        } catch (error) {
+            throw error;
+        }
+    };
+};
+
+const models = new ModelsService(1, 1);
+models.get().then(i => console.log(i));
