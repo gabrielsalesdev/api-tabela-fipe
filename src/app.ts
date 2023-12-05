1e11816608b6c9d3c005c1f342a7412b4e5de237
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-05 11:56:01 -0300

    feat(app.ts): implementando arquivo app

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import carsRouter from './routers/cars.router';
+import motorcyclesRouter from './routers/motorcycles.router';
+import trucksRouter from './routers/trucks.router';
+import ErrorsMiddleware from './middlewares/errors.middleware';
+
+const app = express();
+const errorsMiddleware = new ErrorsMiddleware();
+
+app.use(express.json());
+app.use(carsRouter, motorcyclesRouter, trucksRouter);
+app.use(errorsMiddleware.handler);
+
+export default app;
