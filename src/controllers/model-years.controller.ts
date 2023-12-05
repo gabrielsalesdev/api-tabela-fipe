08a66ff0411875e12a66da1f1e7274121d1c87de
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-05 10:30:04 -0300

    feat(model-years.controller.ts): implementando classe de controller dos anos modelo

---
+++ src/controllers/model-years.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import ModelYearsService from "../services/model-years.service";
+import { ModelYear } from "../interfaces/model-year.interface";
+
+export default class ModelYearsController {
+    public get = (vehicleId: number) => async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { brandId, modelId } = req.params;
+
+            const modelYearsService = new ModelYearsService(vehicleId, Number(brandId), Number(modelId));
+
+            const modelYears: ModelYear[] = await modelYearsService.get();
+
+            res.status(200).json(modelYears);
+        } catch (error) {
+            next(error);
+        }
+    };
+};
