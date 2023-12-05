b7121574a7561130893ab235e633403d7dba57ae
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-05 11:04:33 -0300

    refactor(reference-tables.helper.ts): alterando tipo da função

--- src/helpers/reference-tables.helper.ts
+++ src/helpers/reference-tables.helper.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const getLatest = async (): Promise<number> => {
+const getLatest = async (): Promise<string> => {
     try {
         const { data } = await axios.post('http://veiculos.fipe.org.br/api/veiculos/ConsultarTabelaDeReferencia');
 
         const latestReferenceTable: number = data[0].Codigo;
 
-        return latestReferenceTable;
+        return latestReferenceTable.toString();
     } catch (error) {
         throw error;
     }
