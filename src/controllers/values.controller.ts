00c61ef14a9b38ea45ca39fc563a0b595177259b
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-05 11:33:18 -0300

    feat(values.controller.ts): implementando o controller de valores

---
+++ src/controllers/values.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import ValuesService from "../services/values.service";
+import { Value } from "../interfaces/value.interface";
+
+export default class ModelsController {
+    public getTradicional = (vehicleId: number) => async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { brandId, modelId, modelYearId } = req.params;
+
+            const valueService = new ValuesService();
+
+            const value: Value = await valueService.getTradicional(vehicleId.toString(), brandId, modelId, modelYearId);
+
+            console.log(value);
+
+            res.status(200).json(value);
+        } catch (error) {
+            next(error);
+        }
+    };
+};
