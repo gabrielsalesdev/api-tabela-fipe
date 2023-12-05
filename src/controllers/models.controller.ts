de3ce72eefc12d042db10f13e6e7bb134791eec4
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-05 00:28:07 -0300

    feat(models.controller.ts): implementando o controller de modelos

---
+++ src/controllers/models.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import ModelsService from "../services/models.service";
+import { Model } from "../interfaces/model.interface";
+
+export default class ModelsController {
+    public get = (vehicleId: number) => async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { brandId } = req.params;
+
+            const modelsService = new ModelsService(vehicleId, Number(brandId));
+
+            const models: Model[] = await modelsService.get();
+
+            res.status(200).json(models);
+        } catch (error) {
+            next(error);
+        }
+    };
+};
