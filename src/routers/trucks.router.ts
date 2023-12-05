a95bf307d5080f68ac22a1112434879d5b205b5e
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-05 12:13:30 -0300

    refactor(trucks.router.ts): importando Router do Express

--- src/routers/trucks.router.ts
+++ src/routers/trucks.router.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import { Router } from 'express';
 import BrandsController from '../controllers/brands.controller';
 import ModelsController from '../controllers/models.controller';
 import ModelYearsController from '../controllers/model-years.controller';
 import ValuesController from '../controllers/values.controller';
 
-const trucksRouter = express();
+const trucksRouter = Router();
 const brandsController = new BrandsController();
 const modelsController = new ModelsController();
 const modelYearsController = new ModelYearsController();
