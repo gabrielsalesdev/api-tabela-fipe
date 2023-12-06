220eb5770a7e09a6c542053df8d8f9400793446f
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-06 14:29:40 -0300

    feat(app.ts): adicionando rota do código FIPE

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import carsRouter from './routers/cars.router';
 import motorcyclesRouter from './routers/motorcycles.router';
 import trucksRouter from './routers/trucks.router';
+import fipeRouter from './routers/fipe.router';
 import ErrorsMiddleware from './middlewares/errors.middleware';
 
 const app = express();
 const errorsMiddleware = new ErrorsMiddleware();
 
 app.use(express.json());
-app.use(carsRouter, motorcyclesRouter, trucksRouter);
+app.use(carsRouter, motorcyclesRouter, trucksRouter, fipeRouter);
 app.use(errorsMiddleware.handler);
 
 export default app;
