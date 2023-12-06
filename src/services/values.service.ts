27c668798117d5a5bec2eeca67d2603aae85015d
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-06 14:31:22 -0300

    feat(values.service.ts): implementando funções de requisição do valor pelo código FIPE e ano modelo

--- src/services/values.service.ts
+++ src/services/values.service.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import referenceTablesHelper from '../helpers/reference-tables.helper';
 import errorsHelper from '../helpers/errors.helper';
+import BrandsService from "./brands.service";
+import ModelsService from "./models.service";
 import { ValueRequestTradicional } from "../interfaces/value-request-traditional.interface";
 import { ValueResponse } from "../interfaces/value-response.interface";
 import { Value } from "../interfaces/value.interface";
+import { ValueRequestByFipe } from "../interfaces/value-request-by-fipe.interface";
 
 export default class ValuesService {
     private requestTradicional = async (vehicleId: string, brandId: string, modelId: string, modelYearId: string): Promise<ValueResponse> => {
@@ -50,7 +53,57 @@ export default class ValuesService {
             throw error;
         }
     };
-};
 
-const values = new ValuesService();
-values.getTradicional("3", "102", "5986", "2022-3");
+    private requestByFipeAndModelYear = async (fipeCode: string, modelYearId: string): Promise<{ vehicleId: string, brand: string, model: string }> => {
+        try {
+            let vehicleId: string = '';
+            let brand: string = '';
+            let model: string = '';
+            let code: string = '0';
+
+            for (let i = 1; i <= 3; i++) {
+                const body: ValueRequestByFipe = {
+                    codigoTabelaReferencia: await referenceTablesHelper.getLatest(),
+                    codigoTipoVeiculo: i.toString(),
+                    anoModelo: modelYearId.split('-')[0],
+                    modeloCodigoExterno: fipeCode,
+                    codigoTipoCombustivel: modelYearId.split('-')[1],
+                    tipoConsulta: "codigo"
+                };
+
+                const { data } = await axios.post('https://veiculos.fipe.org.br/api/veiculos/ConsultarValorComTodosParametros', body);
+
+                if (data.codigo && data.codigo === '2') code = '2';
+
+                if (!data.codigo) {
+                    vehicleId = i.toString();
+                    brand = data.Marca;
+                    model = data.Modelo;
+                }
+            }
+
+            if (!vehicleId) errorsHelper.checkResponseErrors({ codigo: code });
+
+            return { vehicleId, model, brand }
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    public getByFipeAndModelYear = async (fipeCode: string, modelYearId: string): Promise<Value> => {
+        try {
+            const { vehicleId, brand, model } = await this.requestByFipeAndModelYear(fipeCode, modelYearId);
+
+            const brandsService = new BrandsService(vehicleId);
+            const brandId: string = await brandsService.findId(brand);
+
+            const modelsService = new ModelsService(vehicleId, brandId);
+            const modelId: string = await modelsService.findId(model);
+
+            const value: Value = await this.getTradicional(vehicleId, brandId, modelId, modelYearId);
+            return value;
+        } catch (error) {
+            throw error;
+        }
+    };
+};
