8057fe50601bafb9bee23bac1658d85952e143b3
Gabriel Sales <gabriel.sales0440@gmail.com>
2023-12-13 11:39:28 -0300

    feat(model-years.service.ts): implementando armazenamento em cache

--- src/services/model-years.service.ts
+++ src/services/model-years.service.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
+import NodeCache from 'node-cache';
 import errorsHelper from '../helpers/errors.helper';
+import dotenvConfig from '../config/dotenv.config';
 import RefereceTablesService from "./reference-tables.service";
 import { ModelYearRequest } from "../interfaces/model-year-request.interface";
 import { ModelYearResponse } from "../interfaces/model-year-response.interface";
 import { ModelYear } from "../interfaces/model-year.interface";
+
+const modelYearsCache = new NodeCache();
+const cacheKey: string = dotenvConfig.cache.key as string;
 
 export default class ModelYearsService {
     vehicleId: string;
@@ -39,6 +44,13 @@ export default class ModelYearsService {
 
     public get = async (): Promise<ModelYear[]> => {
         try {
+            const modelYearsCached = modelYearsCache.get(cacheKey);
+
+            if (modelYearsCached !== undefined && modelYearsCached !== null) {
+                const modelYears: ModelYear[] = modelYearsCached as ModelYear[];
+                if (modelYears[0].idVeiculo === this.vehicleId && modelYears[0].idMarca === this.brandId && modelYears[0].idModelo === this.modelId) return modelYears;
+            }
+
             const modelYearsResponse: ModelYearResponse[] = await this.request();
 
             const modelYears: ModelYear[] = modelYearsResponse.map(modelYear => {
@@ -50,6 +62,9 @@ export default class ModelYearsService {
                     nomeAnoModelo: modelYear.Label,
                 }
             });
+
+            modelYearsCache.set(cacheKey, modelYears, 86400);
+
             return modelYears;
         } catch (error) {
             throw error;
